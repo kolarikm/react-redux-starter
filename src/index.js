698d6a6ce00003ee3ce5d5e0c14645b927e0d2f5
kolarikm <kolarikm@mail.gvsu.edu>
2018-10-17 21:04:07 -0400

    Boilerplate setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+/**
+ * https://codesandbox.io/s/github/tylerbuchea/my-simple-app/tree/master/?from-embed
+ */
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
