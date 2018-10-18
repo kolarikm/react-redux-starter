a60b8c8f6044bf7a8bcf1dd7343ae78013315b53
Michael Kolarik <michael.kolarik@blujaysolutions.com>
2018-10-18 16:50:43 -0400

    Progress on implementing Redux

---
+++ store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux';
+
+export function configureStore(initialState = {}) {
+    const store = createStore(reducers, initialState);
+    return store;
+}
+
+export const store = configureStore();
